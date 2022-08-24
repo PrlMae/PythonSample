43cf09d9fa196e1019446fa40e002e3b13433a32
PrlMae <111949968+PrlMae@users.noreply.github.com>
2022-08-24 13:05:33 +0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,38 @@
+##print("Hello World!")
+
+# program.py
+
+#sum = 1 + 2
+#print(sum)
+#print("show this in the console")
+
+# python3 hello.py
+
+#sum = 1 + 2 # 3
+#product = sum * 2
+#print(product)
+
+#planets_in_solar_system = 8 # int, pluto used to be the 9th planet, but is too small
+#can_liftoff = True
+#shuttle_landed_on_the_moon = "Apollo 11" #string
+
+#print(type(distance_to_alpha_centauri))
+
+#left_side = 10
+#right_side = 5
+#print(left_side / right_side) # 2
+
+#from datetime import date
+#from unicodedata import name
+#print(date.today())
+
+#print ("The date today is ", str(date.today()))
+
+#print("Welcome to the greeter program")
+#name = input("Enter your name : ")
+#print("Greetings"+ name)
+
+print("calculator program")
+first_number = input("first number: ")
+second_number = input("second number: ")
+print(int(first_number) + int(second_number))
